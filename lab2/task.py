66cb55f21c26018dbd3f622b077dd19f9b1bca81
Shtabik <shtabnydaniilofbsu@gmail.com>
2026-04-21 11:44:05 +0300

    Исправленная(новая) задача классификации и используемый data set

--- lab2/task.py
+++ lab2/task.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression,LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import root_mean_squared_error, mean_absolute_error,classification_report, confusion_matrix
+from sklearn.metrics import root_mean_squared_error,r2_score, mean_absolute_error,classification_report, confusion_matrix
 
 
 df = pd.read_csv('Car_Price.csv')
@@ -14,15 +15,23 @@ y_pred_test = linear_model.predict(X_test)
 
 RMSE = root_mean_squared_error(y_test, y_pred_test)
 MAE = mean_absolute_error(y_test, y_pred_test)
+r2=r2_score(y_test, y_pred_test)
+print(f"R^2:{r2}")
 print(f"RMSE: {RMSE}")
 print(f"MAE: {MAE}")
 print(f"Средняя цена машин:{df['Price'].mean()}")
 
 #задача классификации
-y_cl = (df['Transmission'] == 'Automatic').astype(int)
-X_cl = df[['Price', 'Mileage', 'Engine Size']]
-X_train_cl, X_test_c, y_train_cl, y_test_cl = train_test_split(X_cl, y_cl, test_size=0.4, random_state=42)
-log_model = LogisticRegression()
-log_model.fit(X_train_cl, y_train_cl)
-y_pred_cl = log_model.predict(X_test_c)
-print(classification_report(y_test_cl, y_pred_cl, target_names=['Manual', 'Automatic']))
+df = pd.read_csv('mushrooms.csv')
+X = df.drop(['class', 'odor', 'gill-color', 'stalk-root'], axis=1)
+y = df['class']
+y = (df['class'] == 'p').astype(int)
+
+X = pd.get_dummies(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+model = LogisticRegression(penalty='l1', solver='liblinear', C=0.01)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print("\nОтчет о классификации:\n", classification_report(y_test, y_pred))
+print("Количество обнуленных признаков:", np.sum(model.coef_ == 0))
